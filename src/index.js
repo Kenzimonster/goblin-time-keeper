057eeffec14495fefe174be4b1efd681b6fbf35b
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-06-28 01:26:46 +1000

    base structure set up
    added HTML, JS, CSS files and structured the folders. added basic html and styling and displayed local time.
    imported moment and moment time zones

---
+++ src/index.js
@@ -0,0 +1,7 @@
+alert("GAWBLINS");
+function displayLocalTime() {
+  let local = document.querySelector("#local-time");
+  let time = moment().tz(moment.tz.guess()).format(`LLLL`);
+  local.innerHTML = time;
+}
+displayLocalTime();
