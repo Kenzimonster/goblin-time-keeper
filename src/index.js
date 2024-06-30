6accc9e71a0fb0ee59f5c4cc4761614782c06449
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-06-30 17:51:17 +1000

    added accurate time and dates for city
    added JS code to make date display accurately. deleted placeholder text and edited some divs and spans to make JS work..

--- src/index.js
+++ src/index.js
@@ -4,3 +4,36 @@ function displayLocalTime() {
   local.innerHTML = time;
 }
 displayLocalTime();
+setInterval(displayLocalTime, 1000);
+
+function presets() {
+  //Paris
+  let parisData = document.querySelector("#paris");
+  let parisDate = parisData.querySelector(".date");
+  let parisTime = parisData.querySelector(".hourMin");
+  let parisSec = parisData.querySelector(".seconds");
+  let parisMoment = moment().tz("Europe/Paris");
+  parisDate.innerHTML = parisMoment.format("ddd, MMMM Do, YYYY");
+  parisTime.innerHTML = parisMoment.format("h:mm");
+  parisSec.innerHTML = parisMoment.format(":ss A");
+  //Tokyo
+  let tokyoData = document.querySelector("#tokyo");
+  let tokyoDate = tokyoData.querySelector(".date");
+  let tokyoTime = tokyoData.querySelector(".hourMin");
+  let tokyoSec = tokyoData.querySelector(".seconds");
+  let tokyoMoment = moment().tz("Asia/Tokyo");
+  tokyoDate.innerHTML = tokyoMoment.format("ddd, MMMM Do, YYYY");
+  tokyoTime.innerHTML = tokyoMoment.format("h:mm");
+  tokyoSec.innerHTML = tokyoMoment.format(":ss A");
+  //Honolulu
+  let hawaiiData = document.querySelector("#hawaii");
+  let hawaiiDate = hawaiiData.querySelector(".date");
+  let hawaiiTime = hawaiiData.querySelector(".hourMin");
+  let hawaiiSec = hawaiiData.querySelector(".seconds");
+  let hawaiiMoment = moment().tz("Pacific/Honolulu");
+  hawaiiDate.innerHTML = hawaiiMoment.format("ddd, MMMM Do, YYYY");
+  hawaiiTime.innerHTML = hawaiiMoment.format("h:mm");
+  hawaiiSec.innerHTML = hawaiiMoment.format(":ss A");
+}
+presets();
+setInterval(presets, 1000);
